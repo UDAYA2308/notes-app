31bc28c6fff9b790d90bf9466abcc7565e93f8b1
udaya2308 <udaya14apr2000@gmail.com>
2020-05-24 09:54:30 +0530

    importing npm modules

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 notes = getNotes()
 
 console.log(notes)
+
+console.log(validator.isEmail('uday@gmail.com'))
+console.log(validator.isURL('https://google.com'))
